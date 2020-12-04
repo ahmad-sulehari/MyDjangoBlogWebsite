e808cdc876d8cd359f4d94efebc0550a3a155e4b
ahmad-sulehari <ahmad.sulehari@gmail.com>
2020-12-04 16:05:41 +0500

    completly working blog website

--- myblog/articles/views.py
+++ myblog/articles/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView, DetailView
 # Create your views here.
+from .models import Articles
+
+class ArticleListView(ListView):
+    model = Articles
+    template_name = 'home.html'
+
+class ArticleDetailView(DetailView):
+    model = Articles
+    template_name = 'detail.html'
